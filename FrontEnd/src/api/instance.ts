2b6760721697cf41f821f8b8a82bdcde7652738e
박현우 <a40293865@gmail.com>
2023-02-24 22:03:34 +0900

    instance 리팩터링

--- FrontEnd/src/api/instance.ts
+++ FrontEnd/src/api/instance.ts
@@ -6,30 +6,47 @@ import { clearUser } from '../features/auth/userSlice';
 
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
-export const BASE_URL = 'https://i8c103.p.ssafy.io/api';
-// export const BASE_URL = 'http://43.200.35.232:8080/api';
+export const BASE_URL = 'https://--------/api';
 
-export const axiosService = axios.create({ baseURL: BASE_URL });
-const accessToken = sessionStorage.getItem('access-token');
-const refreshToken = sessionStorage.getItem('refresh-token');
+const axiosService = axios.create({ baseURL: BASE_URL });
 
-if (accessToken) axiosService.defaults.headers.common['access-token'] = accessToken;
-if (refreshToken) axiosService.defaults.headers.common['refresh-token'] = refreshToken;
 type CustomHeaders = {
-  'access-token': string;
-  'refresh-token': string;
+  'access-token'?: string;
+  'refresh-token'?: string;
 } & AxiosHeaders;
+
+const setAuthHeaders = (headers: CustomHeaders) => {
+  const accessToken = sessionStorage.getItem('access-token');
+  const refreshToken = sessionStorage.getItem('refresh-token');
+
+  if (accessToken) headers['access-token'] = accessToken;
+  if (refreshToken) headers['refresh-token'] = refreshToken;
+
+  return headers;
+};
+
+const getAccessToken = async () => {
+  const instance = axios.create({ baseURL: BASE_URL });
+
+  const headers: CustomHeaders = {};
+  setAuthHeaders(headers);
+
+  try {
+    const result = await instance.get('users/token/refresh', { headers });
+    const accessToken = result.headers['access-token'];
+    if (accessToken) sessionStorage.setItem('access-token', accessToken);
+    return accessToken;
+  } catch (err) {
+    throw err;
+  }
+};
 
 export const setAxiosConfig = (navigate: NavigateFunction) => {
   axiosService.interceptors.request.use(
     function (config) {
       const headers = config.headers as CustomHeaders;
 
-      const accessToken = sessionStorage.getItem('access-token');
-      const refreshToken = sessionStorage.getItem('refresh-token');
-
-      if (accessToken) headers['access-token'] = accessToken;
-      if (refreshToken) headers['refresh-token'] = refreshToken;
+      setAuthHeaders(headers);
 
       return config;
     },
@@ -43,19 +60,14 @@ export const setAxiosConfig = (navigate: NavigateFunction) => {
       if (res.data.statusCodeValue === 401) {
         // UNAUTHORIZED
         // access-token 만료
-        const instance = axios.create({ baseURL: BASE_URL });
-
-        const accessToken = sessionStorage.getItem('access-token');
-        const refreshToken = sessionStorage.getItem('refresh-token');
-
-        instance.defaults.headers['access-token'] = accessToken;
-        instance.defaults.headers['refresh-token'] = refreshToken;
-
         try {
-          const result = await instance.get('users/token/refresh');
-          const accessToken = result.headers['access-token'];
-          if (accessToken) sessionStorage.setItem('access-token', accessToken);
-          navigate(0); // refresh
+          const accessToken = await getAccessToken();
+          if (accessToken) {
+            const headers: CustomHeaders = {};
+            setAuthHeaders(headers);
+            config.headers = headers;
+            return axiosService.request(res.config);
+          }
         } catch (err) {
           // refresh token 만료
           alert('다시 로그인 해주세요');
